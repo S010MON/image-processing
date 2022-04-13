ebd56538859acd9b478c3e21410f42a565ab5a0f
S010MON <leon@bdebnath.com>
2022-04-13 15:35:54 +0300

    project 1 - ex 1

--- lab1_python/lab_1.py
+++ lab1_python/lab_1.py
@@ -1,8 +1,9 @@
 import cv2
 
 
-img = cv2.imread('pizza1.png')
-cv2.imshow('image', img)
+img_rgb = cv2.imread('pizza1.png')
+
+cv2.imshow('image', img_rgb)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
---
+++ project1/proj_1_test.py
@@ -0,0 +1,52 @@
+import unittest
+from proj_1.proj_1 import rgb_to_hsv
+
+
+class MyTestCase(unittest.TestCase):
+    def test_rgb_hsv_black(self):
+        rgb = (0, 0, 0)
+        exp = (0, 0, 0)
+        act = rgb_to_hsv(rgb[0], rgb[1], rgb[2])
+        for i in range(len(exp)):
+            self.assertAlmostEqual(exp[i], act[i], delta=1)
+
+    def test_rgb_hsv_white(self):
+        rgb = (255, 255, 255)
+        exp = (0, 0, 100)
+        act = rgb_to_hsv(rgb[0], rgb[1], rgb[2])
+        for i in range(len(exp)):
+            self.assertAlmostEqual(exp[i], act[i], delta=1)
+
+    def test_rgb_hsv_red(self):
+        rgb = (255, 0, 0)
+        exp = (0, 100, 100)
+        act = rgb_to_hsv(rgb[0], rgb[1], rgb[2])
+        for i in range(len(exp)):
+            self.assertAlmostEqual(exp[i], act[i], delta=1)
+
+    def test_rgb_hsv_green(self):
+        rgb = (0, 255, 0)
+        exp = (120, 100, 100)
+        act = rgb_to_hsv(rgb[0], rgb[1], rgb[2])
+        for i in range(len(exp)):
+            self.assertAlmostEqual(exp[i], act[i], delta=1)
+
+    def test_rgb_hsv_blue(self):
+        rgb = (0, 0, 255)
+        exp = (240, 100, 100)
+        act = rgb_to_hsv(rgb[0], rgb[1], rgb[2])
+        for i in range(len(exp)):
+            self.assertAlmostEqual(exp[i], act[i], delta=1)
+
+    def test_rgb_hsv_rand(self):
+        rgb = (35, 60, 68)
+        exp = (195, 49, 27)
+        act = rgb_to_hsv(rgb[0], rgb[1], rgb[2])
+        for i in range(len(exp)):
+            self.assertAlmostEqual(exp[i], act[i], delta=1)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
