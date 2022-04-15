d9c88d41add9661d9a58c3f7ed5758b3e0707f31
S010MON <leon@bdebnath.com>
2022-04-15 14:08:44 +0300

    Ex2 refactor

--- project1/ex_2.py
+++ project1/ex_2.py
@@ -19,41 +19,48 @@ def negative(img):
 
 
 def contrast(img, gamma):
-
     img = np.divide(img, 255)
     return np.power(img, 1/gamma)
 
 
-if __name__ == '__main__':
+def histograms():
     img1 = cv2.imread('images/shadows_small.png')
     img2 = cv2.imread('images/fog_small.png')
-
     data_img1 = histogram(img1)
     data_neg1 = histogram(negative(img1))
     data_img2 = histogram(img2)
     data_neg2 = histogram(negative(img2))
 
-    # plt.subplot(2, 2, 1)
-    # plt.hist(data_img1, bins=255)
-    # plt.title('Shadows Greyscale')
-    #
-    # plt.subplot(2, 2, 2)
-    # plt.title('Shadows Negative')
-    # plt.hist(data_neg1, bins=255)
-    #
-    # plt.subplot(2, 2, 3)
-    # plt.title('Fog Greyscale')
-    # plt.hist(data_img2, bins=255)
-    #
-    # plt.subplot(2, 2, 4)
-    # plt.title('Fog Negative')
-    # plt.hist(data_neg2, bins=255)
-    # plt.show()
+    plt.subplot(2, 2, 1)
+    plt.hist(data_img1, bins=255)
+    plt.title('Shadows Greyscale')
 
-    cv2.imshow('Original', img1)
-    cv2.imshow('Negative', negative(img1))
-    cv2.imshow('Low Contrast', contrast(img1, 1.7))
-    cv2.imshow('High Contrast', contrast(img1, 0.3))
+    plt.subplot(2, 2, 2)
+    plt.title('Shadows Negative')
+    plt.hist(data_neg1, bins=255)
 
+    plt.subplot(2, 2, 3)
+    plt.title('Fog Greyscale')
+    plt.hist(data_img2, bins=255)
+
+    plt.subplot(2, 2, 4)
+    plt.title('Fog Negative')
+    plt.hist(data_neg2, bins=255)
+    plt.show()
+
+
+def contrasts(img_path: str):
+
+    img = cv2.imread(img_path)
+    cv2.imshow('Original', img)
+    cv2.imshow('Negative', negative(img))
+    cv2.imshow('Low Contrast', contrast(img, 1.5))
+    cv2.imshow('High Contrast', contrast(img, 0.5))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    histograms()
+    contrasts('images/fog_small.png')
+    contrasts('images/shadows_small.png')
