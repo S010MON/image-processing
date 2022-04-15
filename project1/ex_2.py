b9103dfc71fe85872d4656b5e32c13adc9749358
S010MON <leon@bdebnath.com>
2022-04-15 13:18:21 +0300

    Ex2 complete

--- project1/ex_2.py
+++ project1/ex_2.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def histogram(img):
-    img = img[:,:,1]
+    img = img[:, :, 1]
 
     data = []
     for line in img:
@@ -14,51 +14,46 @@ def histogram(img):
     return data
 
 
-def pointwise(img):
+def negative(img):
+    return 255 - img
 
-    img_out = np.zeros(np.shape(img))
-    for x in range(len(img)):
-        for y in range(len(img[x])):
 
-            s = 100 - 1 - img[x][y][0]
-            img_out[x][y][0] = s
-            img_out[x][y][1] = s
-            img_out[x][y][2] = s
+def contrast(img, gamma):
 
-    return img_out
+    img = np.divide(img, 255)
+    return np.power(img, 1/gamma)
 
 
 if __name__ == '__main__':
     img1 = cv2.imread('images/shadows_small.png')
-    neg1 = pointwise(img1)
-
     img2 = cv2.imread('images/fog_small.png')
-    neg2 = pointwise(img2)
 
     data_img1 = histogram(img1)
-    data_neg1 = histogram(neg1)
+    data_neg1 = histogram(negative(img1))
     data_img2 = histogram(img2)
-    data_neg2 = histogram(neg2)
+    data_neg2 = histogram(negative(img2))
 
-    plt.subplot(2, 2, 1)
-    plt.hist(data_img1, bins=255)
-    plt.title('Shadows Greyscale')
-
-    plt.subplot(2, 2, 2)
-    plt.title('Shadows Negative')
-    plt.hist(data_neg1, bins=255)
-
-    plt.subplot(2, 2, 3)
-    plt.title('Fog Greyscale')
-    plt.hist(data_img2, bins=255)
-
-    plt.subplot(2, 2, 4)
-    plt.title('Fog Negative')
-    plt.hist(data_neg2, bins=255)
-    plt.show()
+    # plt.subplot(2, 2, 1)
+    # plt.hist(data_img1, bins=255)
+    # plt.title('Shadows Greyscale')
+    #
+    # plt.subplot(2, 2, 2)
+    # plt.title('Shadows Negative')
+    # plt.hist(data_neg1, bins=255)
+    #
+    # plt.subplot(2, 2, 3)
+    # plt.title('Fog Greyscale')
+    # plt.hist(data_img2, bins=255)
+    #
+    # plt.subplot(2, 2, 4)
+    # plt.title('Fog Negative')
+    # plt.hist(data_neg2, bins=255)
+    # plt.show()
 
     cv2.imshow('Original', img1)
-    cv2.imshow('Negative', neg1)
+    cv2.imshow('Negative', negative(img1))
+    cv2.imshow('Low Contrast', contrast(img1, 1.7))
+    cv2.imshow('High Contrast', contrast(img1, 0.3))
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
