bfd57bf40457db44395688256859a88c8f6eb076
S010MON <leon_debnath@hotmail.co.uk>
2022-04-26 14:33:33 +0200

    complete ex2 write up

--- project1/ex_2.py
+++ project1/ex_2.py
@@ -1,5 +1,4 @@
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -15,38 +14,12 @@ def histogram(img):
 
 
 def negative(img):
-    return 255 - img
+    return 255 - int(img)
 
 
 def contrast(img, gamma):
     img = np.divide(img, 255)
     return np.power(img, 1/gamma)
-
-
-def histograms():
-    img1 = cv2.imread('images/shadows_small.png')
-    img2 = cv2.imread('images/fog_small.png')
-    data_img1 = histogram(img1)
-    data_neg1 = histogram(negative(img1))
-    data_img2 = histogram(img2)
-    data_neg2 = histogram(negative(img2))
-
-    plt.subplot(2, 2, 1)
-    plt.hist(data_img1, bins=255)
-    plt.title('Shadows Greyscale')
-
-    plt.subplot(2, 2, 2)
-    plt.title('Shadows Negative')
-    plt.hist(data_neg1, bins=255)
-
-    plt.subplot(2, 2, 3)
-    plt.title('Fog Greyscale')
-    plt.hist(data_img2, bins=255)
-
-    plt.subplot(2, 2, 4)
-    plt.title('Fog Negative')
-    plt.hist(data_neg2, bins=255)
-    plt.show()
 
 
 def contrasts(img_path: str):
@@ -61,6 +34,5 @@ def contrasts(img_path: str):
 
 
 if __name__ == '__main__':
-    histograms()
     contrasts('images/fog_small.png')
     contrasts('images/shadows_small.png')
