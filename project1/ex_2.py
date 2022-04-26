e005dc43c7010b15d47a700bc4ae9fb334cbc853
S010MON <leon_debnath@hotmail.co.uk>
2022-04-26 14:34:02 +0200

    remove unused imports

--- project1/ex_2.py
+++ project1/ex_2.py
@@ -1,4 +1,3 @@
-import cv2
 import numpy as np
 
 
@@ -21,18 +20,3 @@ def contrast(img, gamma):
     img = np.divide(img, 255)
     return np.power(img, 1/gamma)
 
-
-def contrasts(img_path: str):
-
-    img = cv2.imread(img_path)
-    cv2.imshow('Original', img)
-    cv2.imshow('Negative', negative(img))
-    cv2.imshow('Low Contrast', contrast(img, 1.5))
-    cv2.imshow('High Contrast', contrast(img, 0.5))
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-if __name__ == '__main__':
-    contrasts('images/fog_small.png')
-    contrasts('images/shadows_small.png')
